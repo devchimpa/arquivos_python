0673ab837afe1a90cdeabb8e8dad0f22ba9f1b19
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-06-08 17:16:53 -0300

    Create buscando_dns.py

---
+++ buscando_dns.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+import socket
+
+dominio = input(" Alvo: ")
+brute = ["ns1", "ns2", "ns3", "ns4", "www", "ftp", "intranet", "mail"]
+
+for nome in brute:
+	DNS= nome + "." + dominio
+	try:
+		print(DNS + ": " + socket.gethostbyname(DNS))
+		
+	except socket.gaierror:
+		pass
